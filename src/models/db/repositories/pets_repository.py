a957bfb01e21a0c11e8b68983fa1327b69296bb4
csendin <eucaiosendin@gmail.com>
2026-07-20 20:03:53 -0300

    test: add unit tests for repositories

--- src/models/db/repositories/pets_repository.py
+++ src/models/db/repositories/pets_repository.py
@@ -3,10 +3,10 @@ from src.models.db.entities.pets import PetsTable
 from sqlalchemy.orm.exc import NoResultFound
 
 class PetsRepository:
-    def __init__(self, db):
+    def __init__(self, db) -> None:
         self.__db = db
 
-    def list_pets(self) -> List:
+    def list_pets(self) -> List[PetsTable]:
         with self.__db as database:
             try:
                 pets = database.session.query(PetsTable).all()
@@ -14,3 +14,19 @@ class PetsRepository:
                 return pets
             except NoResultFound:
                 return []
+            
+    def delete_pets(self, name: str) -> None:
+        with self.__db as database:
+            try:
+                (
+                    database
+                        .session
+                        .query(PetsTable)
+                        .filter(PetsTable.name == name)
+                        .delete()
+                )
+
+                database.session.commit()
+            except Exception:
+                database.session.rollback()
+                raise Exception
--- src/models/db/repositories/repositories_test.py
+++ src/models/db/repositories/repositories_test.py
@@ -1,10 +1,18 @@
+import pytest
 from src.models.db.settings.connection import database_connection
 from .pets_repository import PetsRepository
 
 database_connection.connect_to_db()
 
+repo = PetsRepository(database_connection)
 
+@pytest.mark.skip(reason='database interaction')
 def test_list_pets():
-    repo = PetsRepository(database_connection)
     response = repo.list_pets()
     print(response)
+
+@pytest.mark.skip(reason='database interaction')
+def test_delete_pet():
+    name = 'belinha'
+
+    repo.delete_pets(name)
